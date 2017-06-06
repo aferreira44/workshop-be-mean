2fde2958b811609ac45476909f4bdd82efdee4b3
André Ferreira <contato@andreferreira.me>
2017-06-06 13:33:37 -0300

    Aula 5 - globals

---
+++ nodejs/globals/process.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const http = require('http')
+
+process.nextTick(() => {
+    console.log('Eu sou um async');
+})
+
+console.log(process.execPath)
+console.log(process.cwd())
+console.log(process.pid)
+
+let server = http.createServer((req, res) => {
+
+})
+
+server.listen(3000, () => {
+    console.log("I'm on")
+})
+
+process.on('SIGINT', () => {
+    console.log('Eu saí, cara')
+    process.exit()
+})
